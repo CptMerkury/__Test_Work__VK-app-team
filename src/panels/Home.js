f9dacb6fab7dd244f8f94c76ef9f2512375dd577
CprMerkury <wsdsss@mail.ru>
2020-11-06 22:45:32 +0300

    Add search block, no filter. Add func logout sex, age

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -5,24 +5,88 @@ import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader
 import Group from '@vkontakte/vkui/dist/components/Group/Group';
 import Cell from '@vkontakte/vkui/dist/components/Cell/Cell';
 import Avatar from '@vkontakte/vkui/dist/components/Avatar/Avatar';
+import {Search} from "@vkontakte/vkui";
 
 const Home = ({id, go, fetchedUser}) => {
         const getSexText = () => {
             switch (fetchedUser.sex) {
-                case 1: return 'female';
-                case 2: return 'male';
-                default: return 'undef';
+                case 1:
+                    return 'female';
+                case 2:
+                    return 'male';
+                default:
+                    return 'undef';
             }
+        }
+        const getAge = (dateString) => {
+            let birth = new Date(dateString);
+            let now = new Date();
+            let beforeBirth = ((() => {
+                birth.setDate(now.getDate());
+                birth.setMonth(now.getMonth());
+                return birth.getTime()
+            })() < birth.getTime()) ? 0 : 1;
+            return now.getFullYear() - birth.getFullYear() - beforeBirth;
         }
         return (
             <Panel id={id}>
-                <PanelHeader>Example</PanelHeader>
+                <PanelHeader>Searching</PanelHeader>
+                <Group>
+                    <Search/>
+                </Group>
                 {fetchedUser &&
                 <Group title="User Data Fetched with VK Bridge">
                     <Cell
                         before={fetchedUser.photo_200 ? <Avatar src={fetchedUser.photo_200}/> : null}
-                        description=
-                            {getSexText()}
+                        description={`${getSexText()} ${getAge(fetchedUser.bdate)}`}
+                    >
+                        {`${fetchedUser.last_name} ${fetchedUser.first_name}`}
+                    </Cell>
+                    <Cell
+                        before={fetchedUser.photo_200 ? <Avatar src={fetchedUser.photo_200}/> : null}
+                        description={`${getSexText()} ${getAge(fetchedUser.bdate)}`}
+                    >
+                        {`${fetchedUser.last_name} ${fetchedUser.first_name}`}
+                    </Cell>
+                    <Cell
+                        before={fetchedUser.photo_200 ? <Avatar src={fetchedUser.photo_200}/> : null}
+                        description={`${getSexText()} ${getAge(fetchedUser.bdate)}`}
+                    >
+                        {`${fetchedUser.last_name} ${fetchedUser.first_name}`}
+                    </Cell>
+                    <Cell
+                        before={fetchedUser.photo_200 ? <Avatar src={fetchedUser.photo_200}/> : null}
+                        description={`${getSexText()} ${getAge(fetchedUser.bdate)}`}
+                    >
+                        {`${fetchedUser.last_name} ${fetchedUser.first_name}`}
+                    </Cell>
+                    <Cell
+                        before={fetchedUser.photo_200 ? <Avatar src={fetchedUser.photo_200}/> : null}
+                        description={`${getSexText()} ${getAge(fetchedUser.bdate)}`}
+                    >
+                        {`${fetchedUser.last_name} ${fetchedUser.first_name}`}
+                    </Cell>
+                    <Cell
+                        before={fetchedUser.photo_200 ? <Avatar src={fetchedUser.photo_200}/> : null}
+                        description={`${getSexText()} ${getAge(fetchedUser.bdate)}`}
+                    >
+                        {`${fetchedUser.last_name} ${fetchedUser.first_name}`}
+                    </Cell>
+                    <Cell
+                        before={fetchedUser.photo_200 ? <Avatar src={fetchedUser.photo_200}/> : null}
+                        description={`${getSexText()} ${getAge(fetchedUser.bdate)}`}
+                    >
+                        {`${fetchedUser.last_name} ${fetchedUser.first_name}`}
+                    </Cell>
+                    <Cell
+                        before={fetchedUser.photo_200 ? <Avatar src={fetchedUser.photo_200}/> : null}
+                        description={`${getSexText()} ${getAge(fetchedUser.bdate)}`}
+                    >
+                        {`${fetchedUser.last_name} ${fetchedUser.first_name}`}
+                    </Cell>
+                    <Cell
+                        before={fetchedUser.photo_200 ? <Avatar src={fetchedUser.photo_200}/> : null}
+                        description={`${getSexText()} ${getAge(fetchedUser.bdate)}`}
                     >
                         {`${fetchedUser.last_name} ${fetchedUser.first_name}`}
                     </Cell>
